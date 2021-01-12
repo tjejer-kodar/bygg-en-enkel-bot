14cad4f0c8f04c20b6fd7e4ac45d1719e333f800
msmalinosterberg <msmalinosterberg@gmail.com>
2021-01-12 23:02:04 +0100

    checks if its saturday & how many days until s-day

--- saturday.js
+++ saturday.js
@@ -1,20 +1,20 @@
-// Här är filen du ska jobba i! En funktion ska svara på
-// om det är lördag idag eller inte och en funktion ska
-// svara på hur många dagar det är kvar.
-
-// Den här funktionen ska returnera true om det är lördag
-// och false om det inte är lördag
 const isItSaturdayToday = (date) => {
-  return
-}
-
-// Denna funktion ska returnera hur många dagar det är
-// kvar till lördag
-const daysUntil = (date) => {
-  return
-}
-
-export {
-  isItSaturdayToday,
-  daysUntil
-}
+  const weekDay = date.getDay(); 
+  if(weekDay === 6) {
+    return true; 
+  }
+    return false; 
+   }
+  
+  
+  const daysUntil = (date) => {
+    const today = date.getDay(); 
+    const daysUntilSaturday  = 6 - today; 
+    return daysUntilSaturday; 
+  }; 
+  
+  export {
+    isItSaturdayToday,
+    daysUntil
+  }
+  
